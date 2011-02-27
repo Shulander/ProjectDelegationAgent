625c77bfeaa15d2af82463133a491919a9fdae28
shulander@gmail.com <shulander@gmail.com@d849d558-d21b-c9b6-bd3d-342510c0ecb4>
2011-02-27 15:13:29 +0000

    ignore list

--- trunk/JadeManager/src/examples/hello/HelloWorldAgent.java
+++ trunk/JadeManager/src/examples/hello/HelloWorldAgent.java
@@ -27,17 +27,19 @@ package examples.hello;
 import jade.core.Agent;
 
 /**
-   This example shows a minimal agent that just prints "Hallo World!" 
-   and then terminates.
-   @author Giovanni Caire - TILAB
+ * This example shows a minimal agent that just prints "Hallo World!" and then
+ * terminates.
+ * 
+ * @author Giovanni Caire - TILAB
  */
 public class HelloWorldAgent extends Agent {
 
-  protected void setup() {
-  	System.out.println("Hello World! My name is "+getLocalName());
-  	
-  	// Make this agent terminate
-  	doDelete();
-  } 
-}
+	@Override
+	protected void setup() {
+		System.out.println("Hello World! My name is " + getLocalName());
 
+		// Make this agent terminate
+		doDelete();
+	}
+	
+}
