885ab5ba6df280680dd22e46928e238d852346c7
lianecafarate@gmail.com <lianecafarate@gmail.com@d849d558-d21b-c9b6-bd3d-342510c0ecb4>
2011-03-03 01:33:43 +0000

    Codigo consumidor

--- trunk/ProdutorConsumidor/src/jade/exemplos/Consumidor.java
+++ trunk/ProdutorConsumidor/src/jade/exemplos/Consumidor.java
@@ -1,15 +1,35 @@
 package jade.exemplos;
 
 import jade.core.*;
+import jade.lang.acl.ACLMessage;
 
 public class Consumidor extends Agent
 {
 	protected void setup () 
 	{
-		//pergunta se o buffer esta vazio
-		//??
-		doWait (); //dorme
-		//se nao, consome do buffer
+		//pergunta estado do buffer
+		enviaMensagem ("estadoBuffer");
+		//recebe a resposta
+		ACLMessage resposta = receive();
+		if (resposta == null) {
+			System.out.println ("Erro ao receber resposta do buffer!");
+			doDelete ();
+		} 				
+		//se buffer vazio
+		if (resposta.getContent() == "vazio") {
+			doWait (); //dorme
+		} else { //se nao
+			//envia nova mensagem pedindo para retirar produto buffer
+			enviaMensagem ("consome");
+		}
+	}
+	
+	void enviaMensagem (String msg)
+	{
+		ACLMessage pergunta = new ACLMessage(ACLMessage.REQUEST);
+		pergunta.setContent (msg);
+		pergunta.addReceiver(new AID("buffer", AID.ISLOCALNAME));
+		send (pergunta);
 	}
 
 }
