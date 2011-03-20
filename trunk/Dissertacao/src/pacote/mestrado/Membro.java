034a14145327523e713493dac7fde16c75edd329
shulander@gmail.com <shulander@gmail.com@d849d558-d21b-c9b6-bd3d-342510c0ecb4>
2011-03-20 22:38:22 +0000

    solicitacao de atividade para o gestor

--- trunk/Dissertacao/src/pacote/mestrado/Membro.java
+++ trunk/Dissertacao/src/pacote/mestrado/Membro.java
@@ -6,188 +6,217 @@ import jade.core.behaviours.CyclicBehaviour;
 import jade.lang.acl.ACLMessage;
 import jade.lang.acl.UnreadableException;
 
+import java.io.IOException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Date;
 import java.util.Hashtable;
+import java.util.LinkedList;
 import java.util.List;
 
 import pacote.mestrado.dominios.TipoHabilidade;
 import pacote.mestrado.dominios.TipoNivel;
 import pacote.mestrado.entidades.Atividade;
 import pacote.mestrado.entidades.Habilidade;
+import pacote.mestrado.entidades.MensagemTO;
+import pacote.mestrado.services.CompatibilidadeTarefaService;
 
 public class Membro extends Agent {
-	private static final long serialVersionUID = -4300677614968664782L;
+    private static final long serialVersionUID = -4300677614968664782L;
 
-	private int passo = 0;
-	private int id;
-	private String nome;
-	private Collection<Habilidade> habilidades; // habilidades que a pessoa
-												// possui
-	private Hashtable<Date, Atividade> agenda; // tempo disponivel da pessoa
-	private double salario; // homem/hora
+    private int passo = 0;
+    private int id;
+    private String nome;
 
-	protected void setup() {
-		System.out.println("Agente " + getAID().getLocalName() + " vivo! :)");
-		inicializaMembro();
-		addBehaviour(new Plano());
+    private Atividade atividadeEscolhida;
+    private Collection<Atividade> atividadesInvalidas;
+    private Collection<Habilidade> habilidades; // habilidades que a pessoa
+						// possui
+    private Hashtable<Date, Atividade> agenda; // tempo disponivel da pessoa
+    private double salario; // homem/hora
+
+    protected void setup() {
+	System.out.println("Agente " + getAID().getLocalName() + " vivo! :)");
+	inicializaMembro();
+	addBehaviour(new Plano());
+    }
+
+    public void inicializaMembro() {
+	atividadesInvalidas = new LinkedList<Atividade>();
+	this.id = 1;
+	this.nome = "Joaninha";
+	habilidades = new ArrayList<Habilidade>();
+	habilidades.add(new Habilidade(1, TipoHabilidade.UML, TipoNivel.SENIOR));
+	habilidades.add(new Habilidade(2, TipoHabilidade.RELACIONAMENTO_CLIENTE, TipoNivel.SENIOR));
+	habilidades.add(new Habilidade(3, TipoHabilidade.GESTAO_TIME, TipoNivel.MASTER));
+	SimpleDateFormat formatador = new SimpleDateFormat("dd/MM/yyyy hh:mm");
+	Date horario1 = new Date();
+	Date horario2 = new Date();
+	Date horario3 = new Date();
+	Date horario4 = new Date();
+	Date horario5 = new Date();
+	Date horario6 = new Date();
+	Date horario7 = new Date();
+	Date horario8 = new Date();
+
+	try {
+	    horario1 = formatador.parse("05/03/2011 09:00");
+	    horario2 = formatador.parse("05/03/2011 10:00");
+	    horario3 = formatador.parse("05/03/2011 11:00");
+	    horario4 = formatador.parse("05/03/2011 13:00");
+	    horario5 = formatador.parse("05/03/2011 14:00");
+	    horario6 = formatador.parse("05/03/2011 15:00");
+	    horario7 = formatador.parse("05/03/2011 16:00");
+	    horario8 = formatador.parse("05/03/2011 17:00");
+	} catch (ParseException e) {
+	    System.out.println("Problema ao converter horario!");
+	    e.printStackTrace();
 	}
+	agenda = new Hashtable<Date, Atividade>();
+	agenda.put(horario1, new Atividade());
+	agenda.put(horario2, new Atividade());
+	agenda.put(horario3, new Atividade());
+	agenda.put(horario4, new Atividade());
+	agenda.put(horario5, new Atividade());
+	agenda.put(horario6, new Atividade());
+	agenda.put(horario7, new Atividade());
+	agenda.put(horario8, new Atividade());
+	System.out.println(agenda);
 
-	public void inicializaMembro() {
-		this.id = 1;
-		this.nome = "Joaninha";
-		habilidades = new ArrayList<Habilidade>();
-		habilidades.add(new Habilidade(1, TipoHabilidade.UML, TipoNivel.SENIOR));
-		habilidades.add(new Habilidade(2,TipoHabilidade.RELACIONAMENTO_CLIENTE, TipoNivel.SENIOR));
-		habilidades.add(new Habilidade(3, TipoHabilidade.GESTAO_TIME,TipoNivel.MASTER));
-		SimpleDateFormat formatador = new SimpleDateFormat("dd/MM/yyyy hh:mm");
-		Date horario1 = new Date();
-		Date horario2 = new Date();
-		Date horario3 = new Date();
-		Date horario4 = new Date();
-		Date horario5 = new Date();
-		Date horario6 = new Date();
-		Date horario7 = new Date();
-		Date horario8 = new Date();
+	this.salario = 20;
+    }
 
-		try {
-			horario1 = formatador.parse("05/03/2011 09:00");
-			horario2 = formatador.parse("05/03/2011 10:00");
-			horario3 = formatador.parse("05/03/2011 11:00");
-			horario4 = formatador.parse("05/03/2011 13:00");
-			horario5 = formatador.parse("05/03/2011 14:00");
-			horario6 = formatador.parse("05/03/2011 15:00");
-			horario7 = formatador.parse("05/03/2011 16:00");
-			horario8 = formatador.parse("05/03/2011 17:00");
-		} catch (ParseException e) {
-			System.out.println("Problema ao converter horario!");
-			e.printStackTrace();
+    private class Plano extends CyclicBehaviour {
+	private static final long serialVersionUID = -8069717410691786862L;
+
+	public void action() {
+	    System.out.println(getAID().getLocalName() + " - Passo: " + passo);
+	    List<Atividade> lista = new ArrayList<Atividade>();
+	    try {
+		switch (passo) {
+		case 0:
+		    solicitaListaAtividades();
+		    break;
+		case 1:
+		    lista = recebeListaAtividades();
+		    if (!lista.isEmpty()) {
+			System.out.println(getAID().getLocalName() + " recebeu lista de atividades");
+			// escolher atividade compativel com a do membro
+			atividadeEscolhida = CompatibilidadeTarefaService.selecionaAtividadeHabilidade(lista,
+				getHabilidades(), atividadesInvalidas);
+
+			notificaGestor(atividadeEscolhida);
+
+			/*
+			 * System.out.println(getAID().getLocalName()+
+			 * " - Lista de Atividades"); //percorre lista de
+			 * atividades for (Atividade at : lista) { at.imprime
+			 * (); } System.out.println
+			 * ("-------------------------------------------");
+			 */
+		    } else {
+			System.out.println(getAID().getLocalName() + "Lista de atividades está vazia! WTF :(");
+		    }
+		    break;
+		case 2:
+		    // TODO: recebe a confirmacao do gestor da alocacao da
+		    recebeConfirmacaoAtividadeEscolhida();
+		    break;
+		default:
+		    doDelete();
 		}
-		agenda = new Hashtable<Date, Atividade>();
-		agenda.put(horario1, new Atividade());
-		agenda.put(horario2, new Atividade());
-		agenda.put(horario3, new Atividade());
-		agenda.put(horario4, new Atividade());
-		agenda.put(horario5, new Atividade());
-		agenda.put(horario6, new Atividade());
-		agenda.put(horario7, new Atividade());
-		agenda.put(horario8, new Atividade());
-		System.out.println(agenda);
-
-		this.salario = 20;
+	    } catch (IOException e) {
+		System.out.println("Erro ao notificar o Gestor");
+		System.exit(1);
+	    }
+	    ++passo;
 	}
 
-	private class Plano extends CyclicBehaviour {
-		private static final long serialVersionUID = -8069717410691786862L;
-
-		public void action() {
-			System.out.println(getAID().getLocalName() + " - Passo: " + passo);
-			List<Atividade> lista = new ArrayList<Atividade>();
-			switch (passo) {
-			case 0:
-				solicitaListaAtividades();
-				break;
-			case 1:
-				lista = recebeListaAtividades();
-				if (!lista.isEmpty()) {
-					System.out.println(getAID().getLocalName()
-							+ " recebeu lista de atividades");
-					/*
-					 * System.out.println(getAID().getLocalName()+
-					 * " - Lista de Atividades"); //percorre lista de atividades
-					 * for (Atividade at : lista) { at.imprime (); }
-					 * System.out.println
-					 * ("-------------------------------------------");
-					 */
-				} else {
-					System.out.println(getAID().getLocalName()
-							+ "Lista de atividades está vazia! WTF :(");
-				}
-				break;
-			case 2:
-				// escolher atividade compativel com a do membro
-				buscarAtividade();
-				break;
-			default:
-				doDelete();
-			}
-			++passo;
-		}
-	}
-
-
-	public void buscarAtividade() {
+	private void recebeConfirmacaoAtividadeEscolhida() {
+	    // TODO Auto-generated method stub
 
 	}
 
-	protected void takeDown() {
-		System.out
-				.println("Agente " + getAID().getLocalName() + " está morto.");
+	private void notificaGestor(Atividade atividadeEscolhida) throws IOException {
+	    MensagemTO mensagem = new MensagemTO();
+	    ACLMessage consulta = new ACLMessage(ACLMessage.REQUEST);
+	    mensagem.setAssunto("notificaGestor");
+	    mensagem.setMensagem(atividadeEscolhida);
+	    consulta.setContentObject(mensagem);
+	    consulta.addReceiver(new AID("gestor", AID.ISLOCALNAME));
+	    send(consulta);
+	    System.out.println(getAID().getLocalName() + ": Enviei mensagem ao gestor.");
 	}
+    }
 
-	public void solicitaListaAtividades() {
-		ACLMessage consulta = new ACLMessage(ACLMessage.REQUEST);
-		consulta.setContent("ListaAtividades");
-		consulta.addReceiver(new AID("gestor", AID.ISLOCALNAME));
-		send(consulta);
-		System.out.println(getAID().getLocalName()
-				+ ": Enviei mensagem ao gestor.");
-	}
+    public void buscarAtividade() {
 
-	@SuppressWarnings("unchecked")
-	public List<Atividade> recebeListaAtividades() {
-		System.out.println(getAID().getLocalName()
-				+ ": Esperando receber lista de atividades do gestor.");
-		ACLMessage resposta = blockingReceive();
-		List<Atividade> listaAtividades = new ArrayList<Atividade>();
-		if (resposta == null) {
-			System.out.println(getAID().getLocalName()
-					+ ": Erro ao receber a lista de atividades do gestor!");
-		} else {
-			try {
-				listaAtividades = (List<Atividade>) resposta.getContentObject();
-			} catch (UnreadableException e) {
-				System.out
-						.println("Problema ao converter a lista de atividades");
-				e.printStackTrace();
-			}
-		}
-		return listaAtividades;
-	}
+    }
 
-	public int getId() {
-		return id;
-	}
+    protected void takeDown() {
+	System.out.println("Agente " + getAID().getLocalName() + " está morto.");
+    }
 
-	public void setId(int id) {
-		this.id = id;
-	}
+    public void solicitaListaAtividades() throws IOException {
+	MensagemTO mensagem = new MensagemTO();
+	ACLMessage consulta = new ACLMessage(ACLMessage.REQUEST);
+	mensagem.setAssunto("ListaAtividades");
+	consulta.setContentObject(mensagem);
+	consulta.addReceiver(new AID("gestor", AID.ISLOCALNAME));
+	send(consulta);
+	System.out.println(getAID().getLocalName() + ": Enviei mensagem ao gestor.");
+    }
 
-	public Collection<Habilidade> getHabilidades() {
-		return habilidades;
+    @SuppressWarnings("unchecked")
+    public List<Atividade> recebeListaAtividades() {
+	System.out.println(getAID().getLocalName() + ": Esperando receber lista de atividades do gestor.");
+	ACLMessage resposta = blockingReceive();
+	List<Atividade> listaAtividades = new ArrayList<Atividade>();
+	if (resposta == null) {
+	    System.out.println(getAID().getLocalName() + ": Erro ao receber a lista de atividades do gestor!");
+	} else {
+	    try {
+		listaAtividades = (List<Atividade>) resposta.getContentObject();
+	    } catch (UnreadableException e) {
+		System.out.println("Problema ao converter a lista de atividades");
+		e.printStackTrace();
+	    }
 	}
+	return listaAtividades;
+    }
 
-	public void setHabilidades(Collection<Habilidade> habilidades) {
-		this.habilidades = habilidades;
-	}
+    public int getId() {
+	return id;
+    }
 
-	public double getSalario() {
-		return salario;
-	}
+    public void setId(int id) {
+	this.id = id;
+    }
 
-	public void setSalario(double salario) {
-		this.salario = salario;
-	}
+    public Collection<Habilidade> getHabilidades() {
+	return habilidades;
+    }
 
-	public String getNome() {
-		return nome;
-	}
+    public void setHabilidades(Collection<Habilidade> habilidades) {
+	this.habilidades = habilidades;
+    }
 
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
+    public double getSalario() {
+	return salario;
+    }
+
+    public void setSalario(double salario) {
+	this.salario = salario;
+    }
+
+    public String getNome() {
+	return nome;
+    }
+
+    public void setNome(String nome) {
+	this.nome = nome;
+    }
 
 }
--- trunk/Dissertacao/src/pacote/mestrado/entidades/Habilidade.java
+++ trunk/Dissertacao/src/pacote/mestrado/entidades/Habilidade.java
@@ -6,59 +6,59 @@ import pacote.mestrado.dominios.TipoHabilidade;
 import pacote.mestrado.dominios.TipoNivel;
 
 public class Habilidade implements Serializable {
-	private static final long serialVersionUID = 8586683260521276748L;
+    private static final long serialVersionUID = 8586683260521276748L;
 
-	private int id;
-	private TipoHabilidade tipo;
-	private TipoNivel nivel;
-	private Integer xp;
+    private int id;
+    private TipoHabilidade tipo;
+    private TipoNivel nivel;
+    private Integer xp;
 
-	public Habilidade(int id, TipoHabilidade tipo, TipoNivel nivel) {
-		this.id = id;
-		this.tipo = tipo;
-		this.nivel = nivel;
-	}
+    public Habilidade(int id, TipoHabilidade tipo, TipoNivel nivel) {
+	this.id = id;
+	this.tipo = tipo;
+	this.nivel = nivel;
+    }
 
-	public int getId() {
-		return id;
-	}
+    public int getId() {
+	return id;
+    }
 
-	public void setId(int id) {
-		this.id = id;
-	}
+    public void setId(int id) {
+	this.id = id;
+    }
 
-	public TipoHabilidade getTipo() {
-		return tipo;
-	}
+    public TipoHabilidade getTipo() {
+	return tipo;
+    }
 
-	public void setTipo(TipoHabilidade tipo) {
-		this.tipo = tipo;
-	}
+    public void setTipo(TipoHabilidade tipo) {
+	this.tipo = tipo;
+    }
 
-	public TipoNivel getNivel() {
-		return nivel;
-	}
+    public TipoNivel getNivel() {
+	return nivel;
+    }
 
-	public void setNivel(TipoNivel nivel) {
-		this.nivel = nivel;
-	}
+    public void setNivel(TipoNivel nivel) {
+	this.nivel = nivel;
+    }
 
-	public void setXp(Integer xp) {
-		this.xp = xp;
-	}
+    public void setXp(Integer xp) {
+	this.xp = xp;
+    }
 
-	public Integer getXp() {
-		return xp;
-	}
+    public Integer getXp() {
+	return xp;
+    }
 
-	public String toString() {
-		StringBuilder str = new StringBuilder();
-		str.append("--Habilidade--"+"\n");
-		str.append("ID: " + this.id+"\n");
-		str.append("Tipo: " + this.tipo+"\n");
-		str.append("Nível: " + this.nivel+"\n");
-		str.append("--------------");
-		return str.toString();
-	}
+    public String toString() {
+	StringBuilder str = new StringBuilder();
+	str.append("--Habilidade--" + "\n");
+	str.append("ID: " + this.id + "\n");
+	str.append("Tipo: " + this.tipo + "\n");
+	str.append("Nível: " + this.nivel + "\n");
+	str.append("--------------");
+	return str.toString();
+    }
 
 }
