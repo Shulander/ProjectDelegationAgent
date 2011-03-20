f0a49b3271e53cb1c1358683a34d2574b505c89c
shulander <shulander@gmail.com>
2011-03-20 22:38:22 +0000

    solicitacao de atividade para o gestor

--- Dissertacao/src/pacote/mestrado/dominios/TipoNivel.java
+++ Dissertacao/src/pacote/mestrado/dominios/TipoNivel.java
@@ -1,44 +1,43 @@
 package pacote.mestrado.dominios;
 
 public enum TipoNivel {
-	JUNIOR(1, "Junior"), PLENO(2, "Pleno"), SENIOR(3, "Sênior"), MASTER(4,
-			"Master");
+    JUNIOR(1, "Junior"), PLENO(2, "Pleno"), SENIOR(3, "Sênior"), MASTER(4, "Master");
 
-	private Integer codigo;
-	private String descricao;
+    private Integer codigo;
+    private String descricao;
 
-	private TipoNivel(Integer codigo, String descricao) {
-		this.codigo = codigo;
-		this.descricao = descricao;
-	}
+    private TipoNivel(Integer codigo, String descricao) {
+	this.codigo = codigo;
+	this.descricao = descricao;
+    }
 
-	public TipoNivel obterPorCodigo(Integer codigo) {
-		for (TipoNivel tipo : values()) {
-			if (tipo.codigo.equals(codigo)) {
-				return tipo;
-			}
-		}
-		throw new IllegalArgumentException("O codigo informado (" + codigo
-				+ ") é invalido para a obtenção do TipoNivel");
+    public TipoNivel obterPorCodigo(Integer codigo) {
+	for (TipoNivel tipo : values()) {
+	    if (tipo.codigo.equals(codigo)) {
+		return tipo;
+	    }
 	}
+	throw new IllegalArgumentException("O codigo informado (" + codigo
+		+ ") é invalido para a obtenção do TipoNivel");
+    }
 
-	public String toString() {
-		return descricao;
-	}
-	
-	public Integer getCodigo() {
-		return codigo;
-	}
-	
-	public boolean before(TipoNivel tipo) {
-		return getCodigo() < tipo.getCodigo();
-	}
-	
-	public boolean after(TipoNivel tipo) {
-		return getCodigo() > tipo.getCodigo();
-	}
-	
-	public Integer diferencaNiveis(TipoNivel tipo) {
-		return getCodigo() - tipo.getCodigo();
-	}
+    public String toString() {
+	return descricao;
+    }
+
+    public Integer getCodigo() {
+	return codigo;
+    }
+
+    public boolean before(TipoNivel tipo) {
+	return getCodigo() < tipo.getCodigo();
+    }
+
+    public boolean after(TipoNivel tipo) {
+	return getCodigo() > tipo.getCodigo();
+    }
+
+    public Integer diferencaNiveis(TipoNivel tipo) {
+	return getCodigo() - tipo.getCodigo();
+    }
 }
