890f16cd8d8018dbbb812bc96eb3d4bc574b0a62
shulander <shulander@gmail.com>
2011-03-28 02:44:11 +0000

    versao inicial da negociacao entre os agentes

--- Dissertacao/src/pacote/mestrado/behaviors/NegocianteAtivoBehaviour.java
+++ Dissertacao/src/pacote/mestrado/behaviors/NegocianteAtivoBehaviour.java
@@ -1,7 +1,16 @@
 package pacote.mestrado.behaviors;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import jade.core.AID;
 import jade.core.behaviours.SimpleBehaviour;
+import jade.lang.acl.ACLMessage;
+import jade.lang.acl.UnreadableException;
 import pacote.mestrado.Membro;
+import pacote.mestrado.entidades.Atividade;
+import pacote.mestrado.entidades.MensagemTO;
 
 /**
  * Esse Behaviour se inicia quando o Agente encontrou uma tarefa para realizar,
@@ -26,7 +35,48 @@ public class NegocianteAtivoBehaviour extends SimpleBehaviour {
     @Override
     public void action() {
 	System.out.println("Teste de novo behavior: NegocianteAtivoBehaviour");
+
+	try {
+	    solicitaTrocaAtividade();
+	    recebeConfirmacao();
+	} catch (IOException e) {
+	    System.out.println("Falha ao enviar a mensagem ao Membro");
+	} catch (UnreadableException e) {
+	    System.out.println("Falha ao receber resposta do Membro");
+	}
+
 	terminou = true;
+    }
+
+    private void recebeConfirmacao() throws UnreadableException {
+	ACLMessage resposta = membro.blockingReceive();
+	if (resposta == null) {
+	    System.out.println(membro.getAID().getLocalName() + ": Erro ao receber a confirmacao da troca!");
+	} else {
+	    MensagemTO mensagem = (MensagemTO) resposta.getContentObject();
+	    Boolean trocaAceita = (Boolean) mensagem.getMensagem();
+	    if(trocaAceita) {
+		membro.getAtividadeEscolhida().setMembroNome(membro.getAID().getLocalName());
+		membro.addBehaviour(new NegociantePassivoBehaviour(membro));
+	    } else {
+		membro.getAtividadesInvalidas().add(membro.getAtividadeEscolhida());
+		membro.setAtividadeEscolhida(null);
+		membro.addBehaviour(new BuscaTarefaBehaviour(membro));
+	    }
+	    terminou = true;
+	}
+
+    }
+
+    private void solicitaTrocaAtividade() throws IOException {
+	MensagemTO mensagem = new MensagemTO();
+	ACLMessage consulta = new ACLMessage(ACLMessage.REQUEST);
+	mensagem.setAssunto(membro.getAID().getLocalName());
+	mensagem.setMensagem(membro.getHabilidades());
+	consulta.setContentObject(mensagem);
+	consulta.addReceiver(new AID(membro.getAtividadeEscolhida().getMembroNome(), AID.ISLOCALNAME));
+	membro.send(consulta);
+	System.out.println(membro.getAID().getLocalName() + ": Enviei mensagem ao gestor.");
     }
 
     @Override
--- Dissertacao/src/pacote/mestrado/behaviors/NegociantePassivoBehaviour.java
+++ Dissertacao/src/pacote/mestrado/behaviors/NegociantePassivoBehaviour.java
@@ -1,7 +1,16 @@
 package pacote.mestrado.behaviors;
 
+import java.io.IOException;
+import java.util.Collection;
+
 import pacote.mestrado.Membro;
+import pacote.mestrado.entidades.Habilidade;
+import pacote.mestrado.entidades.MensagemTO;
+import pacote.mestrado.services.CompatibilidadeTarefaService;
+import jade.core.AID;
 import jade.core.behaviours.SimpleBehaviour;
+import jade.lang.acl.ACLMessage;
+import jade.lang.acl.UnreadableException;
 
 /**
  * Esse Behaviour se inicia quando o Agente Membro ja encontrou uma tarefa. Ele
@@ -25,7 +34,66 @@ public class NegociantePassivoBehaviour extends SimpleBehaviour {
     @Override
     public void action() {
 	System.out.println("Teste de novo behavior: NegociantePassivoBehaviour");
+	try {
+	    recebeTrocaAtividade();
+	} catch (IOException e) {
+	    System.out.println("Falha ao enviar a mensagem ao Membro");
+	} catch (UnreadableException e) {
+	    System.out.println("Falha ao receber resposta do Membro");
+	}
+
 	terminou = true;
+    }
+
+    private void recebeTrocaAtividade() throws IOException, UnreadableException {
+	ACLMessage msg = membro.blockingReceive(100);
+	if (msg != null) {
+	    MensagemTO mensagem = (MensagemTO) msg.getContentObject();
+
+	    String nomeRequisicao = mensagem.getAssunto();
+	    Collection<Habilidade> habilidadesRequisicao = (Collection<Habilidade>) mensagem.getMensagem();
+
+	    double minhaPontuacao = CompatibilidadeTarefaService.calculaGrauCompatibilidade(
+		    membro.getAtividadeEscolhida(), membro.getHabilidades());
+	    double pontuacaoRequisicao = CompatibilidadeTarefaService.calculaGrauCompatibilidade(
+		    membro.getAtividadeEscolhida(), habilidadesRequisicao);
+
+	    boolean trocaAceita = minhaPontuacao < pontuacaoRequisicao;
+
+	    // caso a Troca foi aceita, troca o dono da atividade para o nome
+	    // requisitado. nofica o gestor da troca e recomeça a procura
+	    if (trocaAceita) {
+		membro.getAtividadeEscolhida().setMembroNome(nomeRequisicao);
+		notificaGestor();
+		membro.setAtividadeEscolhida(null);
+		membro.addBehaviour(new BuscaTarefaBehaviour(membro));
+		terminou = true;
+	    }
+	    // envia resposta
+	    enviaConfirmacao(msg, trocaAceita);
+	}
+
+    }
+
+    private void notificaGestor() throws IOException {
+	MensagemTO mensagem = new MensagemTO();
+	ACLMessage consulta = new ACLMessage(ACLMessage.REQUEST);
+	mensagem.setAssunto("trocaAtividade");
+	mensagem.setMensagem(membro.getAtividadeEscolhida());
+	consulta.setContentObject(mensagem);
+	consulta.addReceiver(new AID(membro.getAtividadeEscolhida().getMembroNome(), AID.ISLOCALNAME));
+	membro.send(consulta);
+	System.out.println(membro.getAID().getLocalName() + ": Enviei mensagem ao gestor.");
+    }
+
+    private void enviaConfirmacao(ACLMessage msg, boolean trocaAceita) throws IOException {
+	ACLMessage resposta = msg.createReply();
+	MensagemTO msgResposta = new MensagemTO();
+	msgResposta.setAssunto("trocaAceita");
+	msgResposta.setMensagem(trocaAceita);
+	resposta.setContentObject(msgResposta);
+	System.out.println("Membro: enviei a resposta ao " + msg.getSender().getLocalName() + ".");
+	membro.send(resposta);
     }
 
     @Override
